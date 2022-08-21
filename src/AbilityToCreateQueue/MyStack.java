2f4736926af70bc2582a5f9122859b495c9f2b13
nitishpriyadarshi <nitish8152@gmail.com>
2022-08-21 13:14:42 +0530

    UC3

---
+++ src/AbilityToCreateQueue/MyStack.java
@@ -0,0 +1,20 @@
+package AbilityToCreateQueue;
+import java.util.LinkedList;
+public class MyStack {
+ LinkedList<Integer>newlist =new LinkedList<Integer>();
+    public void Push(Integer Data) {
+        newlist.add(Data);
+    }
+    public void peek() {
+        System.out.println(newlist.peek());
+    }
+
+    public void pop() {
+        while(newlist.size()!=0) {
+            newlist.pop();
+        }
+    }
+    public void PrintStack() {
+        System.out.println(newlist);
+    }
+    }
